a69435f31e1c255c98b390133067e321cc2a3131
jdspiral <jdspiral@gmail.com>
2016-09-29 22:08:03 -0400

    Add functions to make it easier to add data to db

--- src/Game/Classes/Database.php
+++ src/Game/Classes/Database.php
@@ -1,12 +1,64 @@
 <?php namespace Game\Classes;
 
+use PDO;
+
 class Database {
 
     protected $db;
 
+    /**
+     * Setup storage in memory
+     */
     function __construct()
     {
-        $this->db = new SQLite3(__DIR__."database/db.sqlite");
+        $dsn = 'sqlite:memory;host=localhost';
+        $this->db= new PDO($dsn);
+    }
+
+
+    /**
+     *
+     */
+    public function add()
+    {
+        // Create table messages with different time format
+        $this->db->exec("CREATE TABLE game_data (
+                      id INTEGER PRIMARY KEY,
+                      player TEXT,
+                      message TEXT)");
+
+        // Array with some test data to insert to database
+        $messages = array('title' => 'Hello!',
+                'message' => 'Just testing...'
+        );
+
+        // Prepare INSERT statement into SQLite3 memory db
+        $insert = "INSERT INTO messages (id, title, message)
+                VALUES (:id, :title, :message)";
+
+        $stmt = $this->db->prepare($insert);
+
+        $title = $messages['title'];
+        $message = $messages['message'];
+
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':message', $message);
+
+        $stmt->execute();
+
+    }
+
+    /**
+     * @return array
+     */
+    public function get()
+    {
+        $query = 'SELECT * FROM messages';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+        return $results;
     }
 
 }
