ca82566acb29fa40118671a5dee4206b37aa4318
jdspiral <jdspiral@gmail.com>
2016-09-30 10:39:28 -0400

    Add ajax and jquery libs

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 <title>Connect Four</title>
     <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="resources/js/scripts.js"></script>
 </head>
 
 <body>
